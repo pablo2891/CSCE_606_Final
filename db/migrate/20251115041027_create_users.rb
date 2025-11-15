981c35a9a6e2325f1433b978dc6bcb103b2f7def
khussalpradhan <pradhankhu@gmail.com>
2025-11-14 22:28:41 -0600

    fixed linting issues in migration files

--- db/migrate/20251115041027_create_users.rb
+++ db/migrate/20251115041027_create_users.rb
@@ -6,19 +6,19 @@ class CreateUsers < ActiveRecord::Migration[7.1]
       t.string :password_digest # For has_secure_password
       t.string :first_name
       t.string :last_name
-      
+
       # --- TAMU Verification ---
       t.boolean :is_tamu_verified, default: false
       t.string :tamu_verification_token, index: { unique: true }
       t.datetime :tamu_verified_at
-      
+
       # --- Profile (Merged) ---
       t.string :headline
       t.text :summary
       t.string :resume_url
       t.string :linkedin_url
       t.string :github_url
-      
+
       # --- Merged Experiences & Educations ---
       t.jsonb :experiences_data, null: false, default: []
       t.jsonb :educations_data, null: false, default: []
--- db/migrate/20251115041052_create_referral_requests.rb
+++ db/migrate/20251115041052_create_referral_requests.rb
@@ -3,18 +3,18 @@ class CreateReferralRequests < ActiveRecord::Migration[7.1]
     create_table :referral_requests do |t|
       t.references :user, null: false, foreign_key: true # The requester
       t.references :referral_post, null: false, foreign_key: true
-      
+
       t.text :note_to_poster # General message
-      
-      t.jsonb :submitted_data, default: {} 
-      
+
+      t.jsonb :submitted_data, default: {}
+
       # For Rails enum: status: { pending: 0, approved: 1, rejected: 2, withdrawn: 3 }
       t.integer :status, default: 0, null: false
 
       t.timestamps
     end
-    
+
     # Ensures a user can't request the same post multiple times
-    add_index :referral_requests, [:user_id, :referral_post_id], unique: true
+    add_index :referral_requests, [ :user_id, :referral_post_id ], unique: true
   end
 end
